f38960895fb242f2b148160836c3aaefd0806f03
vkottler <vkottler@wisc.edu>
2019-01-07 09:07:40 -0600

    fix linting error

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ from blizzard_api import VERSION, DESCRIPTION
 
 # https://packaging.python.org/tutorials/packaging-projects/
 with open("README.md", "r") as fh:
-    long_description = fh.read()
+    LONG_DESCRIPTION = fh.read()
 
 setup(name="blizzard_api",
       version=VERSION,
@@ -24,7 +24,7 @@ setup(name="blizzard_api",
       author="Vaughn Kottler",
       author_email="vaughnkottler@gmail.com",
       description=DESCRIPTION,
-      long_description=long_description,
+      long_description=LONG_DESCRIPTION,
       long_description_content_type="text/markdown",
       url="https://github.com/vkottler/blizzard-api-cli",
       classifiers=["Programming Language :: Python :: 3.5",
